f9580c4db41f24f40b09974882ae936bae654e51
anweshan2001 <53610465+anweshan2001@users.noreply.github.com>
2019-09-02 20:15:28 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,20 @@
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		int a=1,b=2,c;
+		System.out.print(a+" ");
+		System.out.println(b+" ");
+		for(int i=0;i<4000000;i++)
+		{c=a+b;
+		a=b;
+		b=c;
+	if(c%2==0)
+		System.out.println(2  + c);
+		
+		
+		}
+	}
+}
+		
+
